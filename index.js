d7cbd17ecb10fd068f1fdefa5644e544facc9c96
taeyun1010 <taeyun1010@yahoo.ca>
2021-06-09 04:10:17 +0900

    회원가입, 로그인, 로그아웃 기능

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const app = express();
 const port = 5000;
 
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
+const { auth } = require('./middleware/auth');
 const { User } = require('./models/User');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI, {
@@ -16,16 +19,65 @@ mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.error(err));
 
+
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     const user = new User(req.body);
+
     user.save((err, userInfo) => {
         if (err) return res.json({ success: false, err});
         return res.status(200).json({
             success: true
         });
     });    
+});
+
+app.post('/api/users/login', (req, res) => {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            });
+        }
+        else {
+            user.comparePassword(req.body.password, (err, isMatch) => {
+                if (!isMatch)
+                    return res.json({ loginSuccess: false, message: "올바르지 않은 비밀번호 입니다."});
+                user.generateToken((err, user) => {
+                    if (err) return res.status(400).send(err);
+                    res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id });             
+                });
+            });
+        }
+    });
+});
+
+app.get('/api/users/auth', auth, (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    });
+});
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id}, { token: "" },
+    (err, user) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).send({
+            succcess: true
+        });
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
